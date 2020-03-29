e06e46b0d6e8de795e8e2345e897074e71538ca2
NyaNyaXin <chenxin950130@gmail.com>
2020-03-29 12:52:45 +0800

    init

---
+++ src/main/java/com/miaoyin/readingnote/ReadingNoteApplication.java
@@ -0,0 +1,13 @@
+package com.miaoyin.readingnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miaoyin/readingnote/ReadingNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaoyin.readingnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
